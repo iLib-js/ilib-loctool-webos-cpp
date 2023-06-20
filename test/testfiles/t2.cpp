98f6404e6629c39294736ada4a8d5239a70d3e91
Goun Lee <goun.lee@lge.com>
2023-06-20 15:54:03 +0900

    Fix to handle a single quotes properly

--- test/testfiles/t2.cpp
+++ test/testfiles/t2.cpp
@@ -1,3 +1,6 @@
 i18n_test1    = ResBundleAdaptor::Instance().getLocString("You're\n Welcome.");
 i18n_test2     = ResBundleAdaptor::Instance().getLocString("No,\n \t Thanks.");
 i18n_test3    = ResBundleAdaptor::Instance().getLocString("Yes \"yes\".");
+if (pResBundle) {
+    trans1 = pResBundle->getLocString("Do you want to change the settings from \'Digital Sound Output\' to \'Pass Through\' to minimize audio delay while playing game?");
+}
